ac45f1fe34c2290739dcb7603a075c55385c2a51
Ruben Astudillo <ruben.astud@gmail.com>
2022-09-01 14:59:23 -0400

    Add ToJSON instances to generated data types

--- packages/ethereum/default.nix
+++ packages/ethereum/default.nix
@@ -1,5 +1,5 @@
-{ mkDerivation, aeson, base, bytestring, data-default, exceptions
-, generics-sop, hpack, hspec, hspec-contrib, hspec-discover
+{ mkDerivation, aeson, aeson-casing, base, bytestring, data-default
+, exceptions, generics-sop, hspec, hspec-contrib, hspec-discover
 , hspec-expectations, jsonrpc-tinyclient, lib, machines, memory
 , memory-hexstring, microlens, microlens-aeson, mtl, OneTuple
 , relapse, tagged, template-haskell, text, transformers, vinyl
@@ -10,21 +10,19 @@ mkDerivation {
   version = "1.0.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    aeson base bytestring data-default exceptions generics-sop
+    aeson aeson-casing base bytestring data-default exceptions
+    generics-sop jsonrpc-tinyclient machines memory memory-hexstring
+    microlens microlens-aeson mtl OneTuple relapse tagged
+    template-haskell text transformers vinyl web3-crypto web3-solidity
+  ];
+  testHaskellDepends = [
+    aeson aeson-casing base bytestring data-default exceptions
+    generics-sop hspec hspec-contrib hspec-discover hspec-expectations
     jsonrpc-tinyclient machines memory memory-hexstring microlens
     microlens-aeson mtl OneTuple relapse tagged template-haskell text
     transformers vinyl web3-crypto web3-solidity
   ];
-  libraryToolDepends = [ hpack ];
-  testHaskellDepends = [
-    aeson base bytestring data-default exceptions generics-sop hspec
-    hspec-contrib hspec-discover hspec-expectations jsonrpc-tinyclient
-    machines memory memory-hexstring microlens microlens-aeson mtl
-    OneTuple relapse tagged template-haskell text transformers vinyl
-    web3-crypto web3-solidity
-  ];
   testToolDepends = [ hspec-discover ];
-  prePatch = "hpack";
   homepage = "https://github.com/airalab/hs-web3#readme";
   description = "Ethereum support for Haskell Web3 library";
   license = lib.licenses.asl20;
